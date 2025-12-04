d21cf8dc350339f82baf5477f750043a429693a6
JBT319 <>
2025-12-04 00:34:52 -0500

    finished ALU, finished Immediate Generator

--- hdl/components/alu.sv
+++ hdl/components/alu.sv
@@ -1,8 +1,30 @@
 module alu (
    input logic [31:0] srcA,
    input logic [31:0] srcB,
-   input logic [2:0] aluSel,
+   input logic [3:0] aluSel,
    output logic [31:0] aluResult
 );
+
+always_comb begin
+   case (aluSel)
+      4'b0000: begin aluResult = srcA + srcB; end 
+      4'b0001: begin aluResult = srcA << srcB[4:0]; end 
+      4'b0010: begin aluResult = {{31{1'b0}},($signed (srcA) < $signed (srcB) ? 1 : 0)}; end 
+      4'b0011: begin aluResult = srcA; end  // Useless
+      4'b0100: begin aluResult = srcA ^ srcB;  end 
+      4'b0101: begin aluResult = srcA >> srcB[4:0]; end 
+      4'b0110: begin aluResult = srcA | srcB; end 
+      4'b0111: begin aluResult = srcA & srcB; end 
+      4'b1000: begin aluResult = srcA; end // Useless
+      4'b1001: begin aluResult = srcA; end // Useless
+      4'b1010: begin aluResult = srcA; end // Useless
+      4'b1011: begin aluResult = srcA; end // Useless
+      4'b1100: begin aluResult = srcA - srcB;  end 
+      4'b1101: begin aluResult = srcA >>> srcB[4:0]; end 
+      4'b1110: begin aluResult = srcA; end // Useless
+      4'b1111: begin aluResult = srcB; end
+      default: begin aluResult = srcA; end // Useless
+   endcase
+end
 
 endmodule
--- hdl/components/sign_extender.sv
+++ hdl/components/sign_extender.sv
@@ -1,17 +1,26 @@
 module sign_extender (
-   input logic [23:0] extend,
+   input logic [31:0] instruct,
    input logic [2:0] immSrc,
    output logic [31:0] extended
 );
    always_comb begin 
       case (immSrc)
-        2'b00: begin
-            output = {extend[23:4], 12'b0};
+        2'b000: begin // U immediate
+            output = {instruct[31:12], 12'b0};
         end
-        2'b01: begin
-            output = {{20{extend[23]}},extend[22:11]};
+        2'b001: begin // I Immediate
+            output = {{20{instruct[31]}},instruct[30:19]};
         end
-        default: output = {extend[23:4], 12'b0};
+        2'b010: begin // J Type
+            output = { {11{instruct[31]}},instruct[31],instruct[19:12],instruct[20],instruct[30:21], 0};
+        end
+        2'b011: begin // S TYPE
+            output = { {20{instruct[31]}}, instruct[31:25], instruct[11:7]} ;
+        end
+        2'b100: begin // B TYPE
+            output = { { 19 {instruct[20]} }, instruct[31], instruct[7], instruct[30:25], instruct[11:8], 0}
+        end
+        default: output = {instruct[23:4], 12'b0};
       endcase
    end
 endmodule
